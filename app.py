4d66b059511c719031b93232d6918733af87a515
Vladislav <sagreah@gmail.com>
2024-03-18 18:32:28 +0800

    qwe

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 from flask_restx import Api, Resource
+from marshmallow import Schema, fields
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 api = Api(app)
 db = SQLAlchemy(app)
 
@@ -11,43 +14,99 @@ directors_ns = api.namespace('directors')
 genres_ns = api.namespace('genres')
 
 
+class Genres(db.Model):
+    __tablename__ = 'genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
+class Directors(db.Model):
+    __tablename__ = 'director'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
+class Movies(db.Model):
+    __tablename__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String)
+    trailer = db.Column(db.String)
+    year = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'), nullable=False)
+    director_id = db.Column(db.Integer, db.ForeignKey('director.id'), nullable=False)
+
+    genre = relationship('Genres')
+    director = relationship('Directors')
+
+
+class MoviesSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+
+
+class DirectorSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+class GenresSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+movies_schema = MoviesSchema()
+directors_schema = DirectorSchema()
+genres_schema = GenresSchema()
+
+
 @movies_ns.route('/')
-class Movies(Resource):
-    def get(self):
-        return
+class MoviesView(Resource):
+    @staticmethod
+    def get():
+        with app.app_context():
+            return movies_schema.dump(Movies.query.all(), many=True)
 
 
 @movies_ns.route('/<int:mid>')
-class Movie(Resource):
+class MovieView(Resource):
 
     def get(self):
         return
 
 
 @directors_ns.route('/')
-class Directors(Resource):
+class DirectorsView(Resource):
     def get(self):
         return
 
 
 @directors_ns.route('/<int:did>')
-class Director(Resource):
+class DirectorView(Resource):
 
     def get(self):
         return
 
 
 @genres_ns.route('/')
-class Genres(Resource):
+class GenresView(Resource):
     def get(self):
         return
 
 
 @genres_ns.route('/<int:gid>')
-class Genre(Resource):
+class GenreView(Resource):
     def get(self):
         return
 
 
-if 'name' == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
